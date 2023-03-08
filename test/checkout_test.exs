97a74920be3cef15bb76fb8951e8a92e3a5f670e
Andrian Poberezhnyk <andrian.poberezhnyk@prosapient.com>
2023-03-08 22:49:24 +0200

    added tests

--- test/checkout_test.exs
+++ test/checkout_test.exs
@@ -3,6 +3,38 @@ defmodule CheckoutTest do
   doctest Checkout
 
   test "greets the world" do
-    assert Checkout.hello() == :world
+    assert 32.5 ==
+             Checkout.new()
+             |> Checkout.scan(:voucher)
+             |> Checkout.scan(:tshirt)
+             |> Checkout.scan(:mug)
+             |> Checkout.total()
+
+    assert 25.0 ==
+             Checkout.new()
+             |> Checkout.scan(:voucher)
+             |> Checkout.scan(:tshirt)
+             |> Checkout.scan(:voucher)
+             |> Checkout.total()
+
+    assert 81.0 ==
+             Checkout.new()
+             |> Checkout.scan(:tshirt)
+             |> Checkout.scan(:tshirt)
+             |> Checkout.scan(:tshirt)
+             |> Checkout.scan(:voucher)
+             |> Checkout.scan(:tshirt)
+             |> Checkout.total()
+
+    assert 74.5 ==
+             Checkout.new()
+             |> Checkout.scan(:voucher)
+             |> Checkout.scan(:tshirt)
+             |> Checkout.scan(:voucher)
+             |> Checkout.scan(:voucher)
+             |> Checkout.scan(:mug)
+             |> Checkout.scan(:tshirt)
+             |> Checkout.scan(:tshirt)
+             |> Checkout.total()
   end
 end
